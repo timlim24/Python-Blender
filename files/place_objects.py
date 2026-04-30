1aba5539aad91b7f4392676b57631478682a09a2
timlim24 <157886626+timlim24@users.noreply.github.com>
2026-04-30 14:49:15 +0300

    S3-8 Magic Numbers

--- files/place_objects.py
+++ files/place_objects.py
@@ -1,4 +1,17 @@
 import bpy
 import os
+import math
 
 os.system('cls' if os.name == 'nt' else 'clear')
+
+cube_loc = (0,0,1)
+cube_rot = (0, 0, math.radians(30))
+cylinder_loc = (0,0,3)
+cylinder_rot = (math.radians(30), math.radians(90), 0)
+monkey_loc = (0, 0, 4.44)
+monkey_rot = (math.radians(-21), 0, math.radians(150))
+
+
+bpy.ops.mesh.primitive_cube_add(location=cube_loc, rotation=cube_rot)
+bpy.ops.mesh.primitive_cylinder_add(location=cylinder_loc, rotation=cylinder_rot)
+bpy.ops.mesh.primitive_monkey_add(location=monkey_loc, rotation=monkey_rot)
