4497f595d969f936aa17a3bf97475096d4ee3b96
timlim24 <157886626+timlim24@users.noreply.github.com>
2026-05-01 08:47:51 +0300

    S4-11 Multiple Arguments

--- files/script_play.py
+++ files/script_play.py
@@ -1,4 +1,18 @@
 import bpy
 from helper_functions import *
 
-select_all_objects()
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+def main():
+    list_of_items = ['Cube', 'Camera']
+    select_by_name(list_of_items)
+
+def select_by_name(items):
+    print(items)
+    for objs in items:
+        print(objs)
+        bpy.data.objects[objs].select_set(True)
+
+if __name__ == '__main__':
+    main()
