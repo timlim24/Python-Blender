021d0418268138174da316e806f7c02b82411fe7
timlim24 <157886626+timlim24@users.noreply.github.com>
2026-05-01 08:33:23 +0300

    S4-10 Helper Functions

--- files/helper_functions.py
+++ files/helper_functions.py
@@ -3,24 +3,16 @@ import os
 
 os.system('cls' if os.name == 'nt' else 'clear')
 
-def main():
-    deselect_all_objects()
-    select_all_object_type('LIGHT')
-
 def select_all_object_type(obj_type='MESH'):
-    '''
-    '''
     objects = bpy.data.objects
     for obj in objects:
         if obj.type == obj_type:
             obj.select_set(True)
 
+
 def select_all_objects():
     bpy.ops.object.select_all(action='SELECT')
 
+
 def deselect_all_objects():
     bpy.ops.object.select_all(action='DESELECT')
-
-
-if __name__ == '__main__':
-    main()
---
+++ files/script_play.py
@@ -0,0 +1,4 @@
+import bpy
+from helper_functions import *
+
+select_all_objects()
