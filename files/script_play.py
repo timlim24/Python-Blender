0a815dba8d81fb37058e91ccd8199ab5babb06f8
timlim24 <157886626+timlim24@users.noreply.github.com>
2026-05-04 18:14:20 +0300

    S4-15 Error Checking

--- files/script_play.py
+++ files/script_play.py
@@ -13,10 +13,17 @@ def select(*object_names, type=None):
     This function is designed to select multiple
     objects by their name and type.
     '''
-    print(object_names)
-    for objs in object_names:
-        print(objs)
-        all_objects()[objs].select_set(True)
+    # Check if objects exist
+    number_of_objects = len(object_names)
+    if number_of_objects == 0:
+        print('No Objects')
+    else:
+        pass
+
+    # print(object_names)
+    # for objs in object_names:
+    #     print(objs)
+    #     all_objects()[objs].select_set(True)
 
 def all_objects():
     return bpy.data.objects
