bd6ad91077695a5146ad7b4b8e71b627b1a14534
timlim24 <157886626+timlim24@users.noreply.github.com>
2026-05-04 18:07:02 +0300

    S4-13 star args

--- files/script_play.py
+++ files/script_play.py
@@ -6,12 +6,11 @@ os.system('cls' if os.name == 'nt' else 'clear')
 
 
 def main():
-    list_of_items = ['Cube', 'Camera']
-    select_by_name(list_of_items)
+    select_by_name('Cube', 'Camera')
 
-def select_by_name(items):
-    print(items)
-    for objs in items:
+def select_by_name(*object_names):
+    print(object_names)
+    for objs in object_names:
         print(objs)
         all_objects()[objs].select_set(True)
 
