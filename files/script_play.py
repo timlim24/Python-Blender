5a29f692b26b8df4e61aa9e67b71a479d5cd7db4
timlim24 <157886626+timlim24@users.noreply.github.com>
2026-05-05 09:56:17 +0300

    S5-18 Randomly Placing Objects In A Volume

--- files/script_play.py
+++ files/script_play.py
@@ -1,7 +1,8 @@
 import bpy
 import os
+import random
+import math
 
-from math import sin, cos, radians, exp
 from helper_functions import *
 
 
@@ -11,33 +12,26 @@ def main():
     bpy.ops.object.delete(use_global=False)
 
     # Object Parameters
-    object_size = 1
     object_type = 'Cube'
-    number_of_objects = 800
+    object_size = 1
+    number_of_objects = 100
     x = y = z = 0
 
-    # Spiral Parameters
-    spiral_radius = 1
-    angle_between_objects = 360 / number_of_objects
-    spiral_height = 15
-    rotation_factor = 8
+    # Volume Parameters
+    vol_x = 20
+    vol_y = 5
+    vol_z = 5
 
     for obj in range(number_of_objects):
-        placement_angle = rotation_factor * obj * angle_between_objects
-        angle = radians(placement_angle)
 
-        vortex_calculation = exp(obj*.005) * spiral_radius
+        x = random.uniform(-vol_x/2, vol_x/2)
+        y = random.uniform(-vol_y/2, vol_y/2)
+        z = random.uniform(-vol_z/2, vol_z/2)
 
-        x = cos(angle) * vortex_calculation
-        y = sin(angle) * vortex_calculation
+        add_mesh(object_type, size = object_size)
+        object = bpy.context.object
+        object.location = (x,y,z)
 
-        add_mesh(object_type, size = object_size, location = (x,y,z))
-
-        rot_angle = radians(-90 - placement_angle)
-
-        bpy.ops.transform.rotate(value=rot_angle, orient_axis='Z')
-
-        z += spiral_height / number_of_objects
 
 if __name__ == '__main__':
     main()
