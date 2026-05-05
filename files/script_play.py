e94effd09b80fe33c04bbce427a2d053e00ef7e2
timlim24 <157886626+timlim24@users.noreply.github.com>
2026-05-05 09:42:55 +0300

    S5-12 Objects Around A Circle

--- files/script_play.py
+++ files/script_play.py
@@ -8,5 +8,22 @@ def main():
     bpy.ops.object.select_all(action='SELECT')
     bpy.ops.object.delete(use_global=False)
 
+    # Object Parameters
+    object_size = 1
+    object_type = 'Cube'
+    number_of_objects = 12
+    x = y = z = 0
+
+    # Circle Parameters
+    radius = 5
+    angle_between_objects = 360 / number_of_objects
+
+    angle = radians(30)
+
+    x = cos(angle) * radius
+    y = sin(angle) * radius
+
+    add_mesh(object_type, size = object_size, location = (x,y,z))
+
 if __name__ == '__main__':
     main()
