c7debaf75955913eb02e0fdbc39af02546fd089b
timlim24 <157886626+timlim24@users.noreply.github.com>
2026-05-05 09:22:05 +0300

    S5-6 Re Ordering Lists Part

--- files/script_play.py
+++ files/script_play.py
@@ -11,11 +11,22 @@ def main():
     z = 5
     so = bpy.context.selected_objects
 
-    for i, obj in enumerate(so):
-        if i % 2 == 0:
-            obj.hide_viewport = True
-        else:
-            obj.hide_viewport = False
+    def object_name(obj):
+        return obj.name
+
+    sorted_so = sorted(so, key = object_name)
+
+    for i, obj in enumerate(sorted_so):
+        print(obj.name)
+        sorted_so[i].location.z = i * 2
+
+#    for i, obj in enumerate(objs):
+#        objs[i].location.z = i * 2
+#        obj.hide_viewport = False
+#        if i % 2 == 0:
+#            obj.hide_viewport = True
+#        else:
+#            obj.hide_viewport = False
 
 if __name__ == '__main__':
     main()
