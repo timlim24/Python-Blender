4e05af267e82c380d287e1caf06df53afd224cd7
timlim24 <157886626+timlim24@users.noreply.github.com>
2026-05-05 09:12:19 +0300

    S5-4 Setting an Objects Location

--- files/script_play.py
+++ files/script_play.py
@@ -5,7 +5,14 @@ from helper_functions import *
 
 def main():
     clear_console()
-    select(types = 'ALL')
+    # select(types = 'ALL')
+    x = 0
+    y = 0
+    z = 5
+    so = bpy.context.selected_objects
+
+    so[0].location = (x,y,z)
+    so[1].location = (x,y,z)
 
 if __name__ == '__main__':
     main()
