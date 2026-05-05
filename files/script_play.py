18484548683cffe7d12a4ce18ac6639860491803
timlim24 <157886626+timlim24@users.noreply.github.com>
2026-05-05 09:26:39 +0300

    S5-8 Creating a Row of Cubes

--- files/script_play.py
+++ files/script_play.py
@@ -6,24 +6,21 @@ from helper_functions import *
 def main():
     clear_console()
     # select(types = 'ALL')
-    x = 0
-    y = 0
-    z = 5
-    so = bpy.context.selected_objects
 
-    sorted_so = sorted(so, key = lambda obj: obj.name)
+    size = 1
+    base = 6
+    x = y = z = 0.5 * size
 
-    for i, obj in enumerate(sorted_so):
-        print(obj.name)
-        sorted_so[i].location.z = i * 2
+    for width in range(base):
+        add_mesh('Cube', size = size, location = (x,y,z))
+        x += size
 
-#    for i, obj in enumerate(objs):
-#        objs[i].location.z = i * 2
-#        obj.hide_viewport = False
-#        if i % 2 == 0:
-#            obj.hide_viewport = True
-#        else:
-#            obj.hide_viewport = False
+    # so = bpy.context.selected_objects
+    # sorted_so = sorted(so, key = lambda obj: obj.name)
+
+    # for i, obj in enumerate(sorted_so):
+    #     print(obj.name)
+    #     sorted_so[i].location.z = i * 2
 
 if __name__ == '__main__':
     main()
